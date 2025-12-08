d091b96b2df198c62352f4eabae82519342235a9
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-08 15:36:22 +0530

    Authentication Filter implemented in API gateway

---
+++ api-gateway/src/main/java/com/example/api/gateway/config/GatewaySecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.api.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeExchange(exchange -> exchange
+            .anyExchange().permitAll());
+
+        http.headers(headers -> headers.disable());
+
+        return http.build();
+    }
+}
