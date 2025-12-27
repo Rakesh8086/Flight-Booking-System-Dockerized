0900218fe165c8bedbf134265ebd28fd2d837bf6
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-27 11:31:32 +0530

    logging lines added to test automatic re-deployment

--- authentication-service/src/main/java/com/spring/login/SpringLoginApplication.java
+++ authentication-service/src/main/java/com/spring/login/SpringLoginApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringLoginApplication {
 
 	public static void main(String[] args) {
+		System.out.println("=======================================");
+		System.out.println("Testing automatic re-deployment by jenkins");
+		System.out.println("=======================================");
 		SpringApplication.run(SpringLoginApplication.class, args);
 	}
 
